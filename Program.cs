334fce095c7df1d85534342064e07b4065c70a9b
Miriam Chen <45667250+Miriam410@users.noreply.github.com>
2021-04-12 13:05:19 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 31. Solicite un ingreso al usuario y determine si  puede interpretarse como
+                   un intervalo de tiempo*/
+            string Fecha;
+            DateTime SalidaFecha;
+            bool Flag = false;
+            do
+            {
+                Console.WriteLine("Ingrese una fecha (formato: dd/MM/yyyy)");
+                Fecha = Console.ReadLine();
+                if (!DateTime.TryParse(Fecha, out SalidaFecha))
+                {
+                    Console.WriteLine("Ustede no ha ingresado una fecha");
+                }
+                else
+                {
+                    Flag = true;
+                    TimeSpan intervalo = DateTime.Now - SalidaFecha;
+                    Console.WriteLine($"La fecha {DateTime.Now} y {SalidaFecha} " +
+                        $"hay un intervalo de {intervalo.ToString()} ");
+                }
+            } while (Flag==false);
+            Console.WriteLine("Ingrese una tecla para salir");
+            Console.ReadKey();
+        }
+    }
+}
